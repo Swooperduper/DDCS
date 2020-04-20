5dc2d7d321a4a336ab93cc9436c686498d0ae57b
Andrew Finegan <andrew.finegan@gmail.com>
2020-04-20 15:28:00 -0400

    add more frontend event changes

--- src/controllers/events/frontend/S_EVENT_DEAD.ts
+++ src/controllers/events/frontend/S_EVENT_DEAD.ts
@@ -2,67 +2,63 @@
  * DDCS Licensed under AGPL-3.0 by Andrew "Drex" Finegan https://github.com/afinegan/DynamicDCS
  */
 
-const _ = require('lodash');
-const constants = require('../../constants');
-const masterDBController = require('../../db/masterDB');
-const DCSLuaCommands = require('../../player/DCSLuaCommands');
-const unitsStaticsController = require('../../serverToDbSync/unitsStatics');
-const webPushCommands = require('../../socketIO/webPush');
+import * as _ from "lodash";
+import * as constants from "../../constants";
+import * as masterDBController from "../../db";
+import * as DCSLuaCommands from "../../player/DCSLuaCommands";
+import * as unitsStaticsController from "../../serverToDbSync/unitsStatics";
+import * as webPushCommands from "../../socketIO/webPush";
 
-_.set(exports, 'processEventDead', function (serverName, sessionName, eventObj) {
-	var nowTime = new Date().getTime();
-	// Occurs when an object is completely destroyed.
-	masterDBController.unitActions('read', serverName, {unitId: _.get(eventObj, ['data', 'arg3'])})
-		.then(function (iunit) {
-			masterDBController.srvPlayerActions('read', serverName, {sessionName: sessionName})
-				.then(function (playerArray) {
-					var iPlayer;
-					var iCurObj;
-					var curIUnit = _.get(iunit, 0);
-					if (curIUnit) {
+export async function processEventDead(sessionName: string, eventObj: any) {
+    const nowTime = new Date().getTime();
+    // Occurs when an object is completely destroyed.
+    masterDBController.unitActionRead({unitId: eventObj.data.arg3})
+        .then((iunit: any) => {
+            masterDBController.srvPlayerActionsRead({sessionName})
+                .then((playerArray: any) => {
+                    const curIUnit = iunit[0];
+                    if (curIUnit) {
 
-						unitsStaticsController.processUnitUpdates(serverName, sessionName, {action: 'D', data: {name: curIUnit.name}});
+                        unitsStaticsController.processUnitUpdates(sessionName, {action: "D", data: {name: curIUnit.name}});
 
-						iPlayer = _.find(playerArray, {name: _.get(curIUnit, 'playername')});
-						if (iPlayer) {
-							iCurObj = {
-								sessionName: sessionName,
-								eventCode: constants.shortNames[eventObj.action],
-								iucid: _.get(iPlayer, 'ucid'),
-								iName: _.get(curIUnit, 'playername'),
-								displaySide: 'A',
-								roleCode: 'I',
-								msg: 'A: ' + constants.side[_.get(curIUnit, 'coalition')] + ' '+ _.get(curIUnit, 'type') + '('+ _.get(curIUnit, 'playername') +') is dead',
-								groupId: _.get(curIUnit, 'groupId')
-							};
-							if (_.get(iCurObj, 'iucid')) {
-								webPushCommands.sendToAll(serverName, {payload: {action: eventObj.action, data: _.cloneDeep(iCurObj)}});
-								masterDBController.simpleStatEventActions('save', serverName, iCurObj);
-							}
-							masterDBController.srvPlayerActions('clearTempScore', serverName, {_id: _.get(iCurObj, 'iucid'), groupId: _.get(iCurObj, 'groupId')})
-								.catch(function (err) {
-									console.log('line35', err);
-								})
-							;
+                        const iPlayer = _.find(playerArray, {name: curIUnit.playername});
+                        if (iPlayer) {
+                            const iCurObj = {
+                                sessionName,
+                                eventCode: constants.shortNames[eventObj.action],
+                                iucid: iPlayer.ucid,
+                                iName: curIUnit.playername,
+                                displaySide: "A",
+                                roleCode: "I",
+                                msg: "A: " + constants.side[curIUnit.coalition] + " " + curIUnit.type + "(" + curIUnit.playername +
+                                    ") is dead",
+                                groupId: curIUnit.groupId
+                            };
+                            if (iCurObj.iucid) {
+                                webPushCommands.sendToAll({payload: {action: eventObj.action, data: _.cloneDeep(iCurObj)}});
+                                masterDBController.simpleStatEventActionsSave(iCurObj);
+                            }
+                            masterDBController.srvPlayerActionsClearTempScore({_id: iCurObj.iucid, groupId: iCurObj.groupId})
+                                .catch((err) => {
+                                    console.log("line35", err);
+                                })
+                            ;
 
-							if (_.get(constants, 'config.inGameHitMessages', true)) {
-								DCSLuaCommands.sendMesgToAll(
-									serverName,
-									_.get(iCurObj, 'msg'),
-									5,
-									nowTime + _.get(constants, 'time.oneMin', 0)
-								);
-							}
-						}
-					}
-				})
-				.catch(function (err) {
-					console.log('err line45: ', err);
-				})
-			;
-		})
-		.catch(function (err) {
-			console.log('err line140: ', err);
-		})
-	;
-});
+                            if (constants.config.inGameHitMessages) {
+                                DCSLuaCommands.sendMesgToAll(
+                                    iCurObj.msg,
+                                    5,
+                                    nowTime + constants.time.oneMin
+                                );
+                            }
+                        }
+                    }
+                })
+                .catch((err) => {
+                    console.log("err line45: ", err);
+                });
+        })
+        .catch((err) => {
+            console.log("err line140: ", err);
+        });
+}
