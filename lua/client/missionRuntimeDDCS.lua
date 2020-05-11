baea90957dbd4d8a0e99debc9be6b0da93fe05e3
Andrew Finegan <andrew.finegan@gmail.com>
2020-05-11 03:25:01 -0400

    updates for units and statics working

--- lua/client/missionRuntimeDDCS.lua
+++ lua/client/missionRuntimeDDCS.lua
@@ -1,4 +1,19 @@
-env.info("startGameRuntimeDDCS")
+function tprint(tbl, indent)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            env.info(formatting)
+            tprint(v, indent + 1)
+        elseif type(v) == 'boolean' then
+            env.info(formatting .. tostring(v))
+        else
+            env.info(formatting .. tostring(v))
+        end
+    end
+end
+
+env.info("startMissionRuntimeDDCS")
 
 ddcs = {}
 local ddcsHost = "localhost"
@@ -19,6 +34,9 @@ assert(udpClient:setpeername(socket.dns.toip(ddcsHost),ddcsPort))
 local udpMissionRuntime = socket.udp()
 assert(udpMissionRuntime:settimeout(0))
 assert(udpMissionRuntime:setsockname(socket.dns.toip(missionRuntimeHost), missionRuntimePort))
+
+completeUnitAliveNames = {}
+completeStaticAliveNames = {}
 
 -- update groups section
 local unitCache = {}
@@ -61,6 +79,7 @@ local function addGroups(groups, coalition)
                         ["inAir"] = unit:inAir()
                     }
                 }
+                table.insert(completeUnitAliveNames, curUnit.data.name)
                 if (velocity) then
                     curUnit.data.speed = math.sqrt(velocity.x ^ 2 + velocity.z ^ 2)
                 end
@@ -110,6 +129,7 @@ end
 function updateGroups(ourArgument, time)
     unitCnt = 0
     checkUnitDead = {}
+    completeUnitAliveNames = {}
 
     local redGroups = coalition.getGroups(coalition.side.RED)
     if redGroups ~= nil then
@@ -138,7 +158,93 @@ function updateGroups(ourArgument, time)
     return time + DATA_TIMEOUT_SEC
 end
 
+-- update Statics section
+local staticCache = {}
+local staticCnt = 0
+local checkStaticDead = {}
+completeStaticAliveNames = {}
 
+local function addStatics(statics, coalition)
+    for staticIndex = 1, #statics do
+        local static = statics[staticIndex]
+        local staticPosition = static:getPosition()
+        local lat, lon, alt = coord.LOtoLL(staticPosition.p)
+        local unitXYZNorthCorr = coord.LLtoLO(lat + 1, lon)
+        local headingNorthCorr = math.atan2(unitXYZNorthCorr.z - staticPosition.p.z, unitXYZNorthCorr.x - staticPosition.p.x)
+        local heading = math.atan2(staticPosition.x.z, staticPosition.x.x) + headingNorthCorr
+        if heading < 0 then
+            heading = heading + 2 * math.pi
+        end
+        local curStatic = {
+            ["uType"] = "static",
+            ["data"] = {
+                ["name"] = static:getName(),
+                ["lonLatLoc"] = {
+                    lon,
+                    lat
+                },
+                ["alt"] = alt,
+                ["hdg"] = math.floor(heading / math.pi * 180),
+
+            }
+        }
+        table.insert(completeStaticAliveNames, curStatic.data.name)
+        if staticCache[curStatic.data.name] ~= nil then
+            if staticCache[curStatic.data.name].lat ~= lat or staticCache[curStatic.data.name].lon ~= lon then
+                staticCache[curStatic.data.name] = {}
+                staticCache[curStatic.data.name].lat = lat
+                staticCache[curStatic.data.name].lon = lon
+                curStatic.action = "U"
+                udpClient:send(JSON:encode(curStatic))
+            end
+        else
+            staticCache[curStatic.data.name] = {}
+            staticCache[curStatic.data.name].lat = lat
+            staticCache[curStatic.data.name].lon = lon
+            curStatic.data.groupName = curStatic.data.name
+            curStatic.data.category = static:getDesc().category
+            curStatic.data.type = static:getTypeName()
+            curStatic.data.coalition = coalition
+            curStatic.data.country = static:getCountry()
+            curStatic.action = "C"
+            udpClient:send(JSON:encode(curStatic))
+        end
+        checkStaticDead[curStatic.data.name] = 1
+    end
+end
+
+function updateStatics(ourArgument, time)
+    staticCnt = 0
+    checkStaticDead = {}
+    completeStaticAliveNames = {}
+
+
+    local redStatics = coalition.getStaticObjects(1)
+    if redStatics ~= nil then
+        addStatics(redStatics, 1)
+    end
+    local blueStatics = coalition.getStaticObjects(2)
+    if blueStatics ~= nil then
+        addStatics(blueStatics, 2)
+    end
+    for k, v in pairs(staticCache) do
+        if checkStaticDead[k] == nil then
+            local curStatic = {
+                action = "D",
+                uType = "static",
+                data = {
+                    name = k
+                }
+            }
+            udpClient:send(JSON:encode(curStatic))
+            staticCache[k] = nil
+        end
+        if k:split(" #")[1] ~= 'New Static Object' then
+            staticCnt = staticCnt + 1
+        end
+    end
+    return time + DATA_TIMEOUT_SEC
+end
 
 -- command section
 function commandExecute(s)
@@ -171,7 +277,7 @@ end
 
 timer.scheduleFunction(runPerFrame, {}, timer.getTime() + DATA_TIMEOUT_SEC)
 timer.scheduleFunction(updateGroups, {}, timer.getTime() + DATA_TIMEOUT_SEC)
-
+timer.scheduleFunction(updateStatics, {}, timer.getTime() + DATA_TIMEOUT_SEC)
 
 -- world.addEventHandler(ddcs)
 env.info("missionRuntimeDDCS loaded")
