f480e2a6cefaf8ab3944d81d77a272619f3c01b1
Andrew Finegan <andrew.finegan@gmail.com>
2021-03-30 23:52:16 -0400

    lock user side

--- src/controllers/action/sideLock.ts
+++ src/controllers/action/sideLock.ts
@@ -7,34 +7,29 @@ import { dbModels } from "../db";
 import * as typings from "../../typings";
 import {I18nResolver} from "i18n-ts";
 
-
 export async function lockUserToSide(incomingObj: any, lockToSide: number): Promise<void> {
-	console.log("stuff1: ", incomingObj, lockToSide);
-    return new Promise((resolve, reject) => {
-        dbModels.srvPlayerModel.find({_id: incomingObj.from}, async (err: any, serverObj: typings.ISrvPlayers[]) => {
-            if (err) { console.log("err: ", err); reject(err); }
-            const curPly = serverObj[0];
-			const engineCache = ddcsController.getEngineCache();
-			console.log("stuff2: ", curPly, engineCache.i18n);
-			const i18n = new I18nResolver(engineCache.i18n, curPly.lang).translation as any;
-			console.log("stuff3: ", i18n.PLAYERALREADYLOCKEDTOSIDE, i18n.PLAYERISNOWLOCKEDTOSIDE);
-			/*
-            if (curPly && curPly.sideLock !== 0) {
-                const mesg = i18n.PLAYERALREADYLOCKEDTOSIDE.replace("#1", i18n[curPly.sideLock].toUpperCase());
-                await ddcsController.sendMesgToPlayerChatWindow(mesg, curPly.playerId);
-            } else {
-                const mesg = i18n.PLAYERISNOWLOCKEDTOSIDE.replace("#1", i18n[curPly.sideLock].toUpperCase());
-                await ddcsController.sendMesgToPlayerChatWindow(mesg, curPly.playerId);
-                dbModels.srvPlayerModel.updateOne(
-                    {_id: incomingObj.from},
-                    {$set: {sideLock: lockToSide}},
-                    (updateErr: any) => {
-                        if (updateErr) { reject(updateErr); }
-                        resolve();
-                    }
-                );
-            }
-			*/
-        });
+    console.log("stuff1: ", incomingObj, lockToSide);
+    return new Promise(async (resolve, reject) => {
+        const curPlayerArray = await ddcsController.srvPlayerActionsRead({_id: incomingObj.from});
+        const curPly = curPlayerArray[0];
+        const engineCache = ddcsController.getEngineCache();
+        console.log("stuff2: ", curPly, engineCache.i18n);
+        const i18n = new I18nResolver(engineCache.i18n, curPly.lang).translation as any;
+        console.log("stuff3: ", i18n.PLAYERALREADYLOCKEDTOSIDE, i18n.PLAYERISNOWLOCKEDTOSIDE);
+        if (curPly && curPly.sideLock !== 0) {
+            const mesg = i18n.PLAYERALREADYLOCKEDTOSIDE.replace("#1", i18n[curPly.sideLock].toUpperCase());
+            await ddcsController.sendMesgToPlayerChatWindow(mesg, curPly.playerId);
+        } else {
+            const mesg = i18n.PLAYERISNOWLOCKEDTOSIDE.replace("#1", i18n[curPly.sideLock].toUpperCase());
+            await ddcsController.sendMesgToPlayerChatWindow(mesg, curPly.playerId);
+            dbModels.srvPlayerModel.updateOne(
+                {_id: incomingObj.from},
+                {$set: {sideLock: lockToSide}},
+                (updateErr: any) => {
+                    if (updateErr) { reject(updateErr); }
+                    resolve();
+                }
+            );
+        }
     });
 }
