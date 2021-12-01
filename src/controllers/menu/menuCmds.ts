4efd979f5503c8b6edb5fdeb46f6ed11f157dde1
kirkwood364 <jamesk1@hotmail.co.uk>
2021-12-01 21:48:13 +0000

    Added Internal ZU-23's & Bofors

--- src/controllers/menu/menuCmds.ts
+++ src/controllers/menu/menuCmds.ts
@@ -39,7 +39,7 @@ export async function internalCargo(curUnit: any, curPlayer: any, intCargoType: 
         const intCargo = _.split(curUnit.intCargoType, "|");
         const curIntCrateType = intCargo[1];
         const curIntCrateBaseOrigin = intCargo[2];
-        const crateType = (curUnit.coalition === 1) ? "UAZ-469" : "Hummer";
+        let crateType = (curUnit.coalition === 1) ? "UAZ-469" : "Hummer";
         if (curUnit.inAir) {
             await ddcsControllers.sendMesgToGroup(
                 curPlayer,
@@ -72,6 +72,17 @@ export async function internalCargo(curUnit: any, curPlayer: any, intCargoType: 
                             curPlayer,
                             curUnit.groupId,
                             "G: Unloading JTAC, please remain still until it is unloaded",
+                            5
+                        );
+                        setTimeout(() => {unpackInternalCargo(curUnit,curPlayer,curIntCrateType,curBaseObj,i18n,crateType);}, _.random(10,20)*1000);
+                    }
+                    if (curIntCrateType === "LightAAA") {
+                        await ddcsControllers.correctPlayerAircraftDuplicates();
+                        crateType = (curUnit.coalition === 1) ? "ZU-23 Emplacement" : "bofors40";
+                        await ddcsControllers.sendMesgToGroup(
+                            curPlayer,
+                            curUnit.groupId,
+                            "G: Unloading Internal Cargo, please remain stationary until it is unloaded",
                             5
                         );
                         setTimeout(() => {unpackInternalCargo(curUnit,curPlayer,curIntCrateType,curBaseObj,i18n,crateType);}, _.random(10,20)*1000);
@@ -151,7 +162,7 @@ export async function internalCargo(curUnit: any, curPlayer: any, intCargoType: 
             }
         }
     }
-    if (intCargoType === "loadJTAC" || intCargoType === "loadBaseRepair" || intCargoType === "loadCCBuild") {
+    if (intCargoType === "loadJTAC" || intCargoType === "loadBaseRepair" || intCargoType === "loadCCBuild" || intCargoType === "loadLightAAA") {
         if (curUnit.inAir) {
             await ddcsControllers.sendMesgToGroup(
                 curPlayer,
@@ -183,6 +194,23 @@ export async function internalCargo(curUnit: any, curPlayer: any, intCargoType: 
                             curPlayer,
                             curUnit.groupId,
                             "G: " + i18n.PICKEDUPJTACINTERNALCRATE.replace("#1", curBaseName),
+                            5
+                        );
+                        if (await isTroopOnboard(curUnit)){
+                            await setInternalCargoMass(curUnit.name,2000);
+                        }else{
+                            await setInternalCargoMass(curUnit.name,1000);
+                        }
+                    }
+                    if (intCargoType === "loadLightAAA") {
+                        await ddcsControllers.unitActionUpdateByUnitId({
+                            unitId: curUnit.unitId,
+                            intCargoType: "|LightAAA|" + curBaseName + "|"
+                        });
+                        await ddcsControllers.sendMesgToGroup(
+                            curPlayer,
+                            curUnit.groupId,
+                            "G:Picked up light AAA internal crate",
                             5
                         );
                         if (await isTroopOnboard(curUnit)){
@@ -2181,6 +2209,89 @@ export async function unpackInternalCargo(curUnit:any, curPlayer:any, internalCa
                 "G: " + i18n.SPAWNJTACFROMINTERNALCARGO,
                 5
             );
+        } else if (internalCargo == "LightAAA"){
+            await unpackIntCrate(curUnit, curUnit.country, crateType, "", false);
+            await ddcsControllers.unitActionUpdateByUnitId({unitId: curUnit.unitId, intCargoType: ""});
         }
+        
     }
+}
+
+export async function unpackIntCrate(
+    playerUnit: any,
+    country: number,
+    type: string,
+    special: string,
+    mobile: boolean
+) {
+    const curPlayerArray = await ddcsControllers.srvPlayerActionsRead({name: playerUnit.playername});
+    const curPlayer = curPlayerArray[0];
+    const engineCache = ddcsControllers.getEngineCache();
+    await ddcsControllers.srvPlayerActionsUpdateacquisitionsUnpacked(curPlayer);
+    let newSpawnArray: any[] = [];
+    const addHdg = 30;
+    const addSpawnHeading = 119;
+    let SpawnHeading = playerUnit.hdg + 180
+    let curUnitHdg = playerUnit.hdg + 180;
+    let pCountry = country;
+    const virtualGroupID = "DU|" + curPlayer.ucid + "|" + type + "|" + special +
+    "|true|" + mobile + "|" + curPlayer.name + "|";
+    const findUnit = _.find(engineCache.unitDictionary, {_id: type});
+    if (findUnit) {
+        const spawnUnitCount = 1;
+        if ((type === "1L13 EWR" || type === "55G6 EWR" || type === "Dog Ear radar") && playerUnit.coalition === 2) {
+            console.log("EWR: UKRAINE");
+            pCountry = 1;
+        }
+        for (let x = 0; x < spawnUnitCount; x++) {
+            let randInc = _.random(1000000, 9999999);
+            let genName = "DU|" + curPlayer.ucid + "|" + type + "|" + special +
+                "|true|" + mobile + "|" + curPlayer.name + "|";
+            const unitStart = _.cloneDeep(findUnit);
+            if (curUnitHdg > 359) {
+                curUnitHdg = curUnitHdg - 359;
+            }
+            if (SpawnHeading > 359) {
+                SpawnHeading = SpawnHeading - 359;
+            }
+            unitStart.name = genName + (randInc + x);
+            unitStart.groupName = genName + randInc;
+            unitStart.lonLatLoc = ddcsControllers.getLonLatFromDistanceDirection(playerUnit.lonLatLoc, curUnitHdg, 0.003);
+            unitStart.hdg = SpawnHeading * 0.0174533;
+            unitStart.country = pCountry;
+            unitStart.playerCanDrive = mobile || false;
+            unitStart.special = special;
+            unitStart.coalition = playerUnit.coalition;
+            unitStart.virtualGrpName = virtualGroupID
+            newSpawnArray.push(unitStart);
+            curUnitHdg = curUnitHdg + addHdg;
+            SpawnHeading = SpawnHeading +addSpawnHeading;
+            await ddcsControllers.spawnUnitGroup(newSpawnArray, false);
+            newSpawnArray = [];
+            const delUnits = await ddcsControllers.unitActionReadStd({
+                playerOwnerId: curPlayer.ucid,
+                playerCanDrive: mobile || false,
+                isCrate: false,
+                dead: false
+            });
+            let curUnit = 0;
+            const grpGroups = _.groupBy(delUnits, "groupName");
+            const tRem = Object.keys(grpGroups).length - engineCache.config.maxUnitsMoving;
+    
+            for (const gUnitKey of Object.keys(grpGroups)) {
+                if (curUnit <= tRem) {
+                    for (const unit of grpGroups[gUnitKey]) {
+                        await ddcsControllers.unitActionUpdateByUnitId({unitId: unit.unitId, dead: true});
+                        await ddcsControllers.destroyUnit(unit.name, "unit");
+                        console.log("Player has too many units, removing:",unit.name)
+                    }
+                    curUnit++;
+                }
+            }
+        }
+        return true;
+    } else {
+        console.log("Count not find unit: line 1172: ", type);
+        return false;
+    }        
 }
