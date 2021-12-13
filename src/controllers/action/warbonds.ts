4f7a69c9ec435a3d600f30daf6d490f1c72dcaa0
Swooperduper <92326642+Swooperduper@users.noreply.github.com>
2021-12-13 16:39:23 -0600

    Update warbonds.ts

--- src/controllers/action/warbonds.ts
+++ src/controllers/action/warbonds.ts
@@ -29,3 +29,101 @@ export async function checkWarBonds(player: typings.ISrvPlayers): Promise<void> 
         }
     }
 }
+export async function spendWarBonds(
+    player: typings.ISrvPlayers,
+    warBondCost: number,
+    warBondItem: string,
+    itemObj: typings.IUnit
+): Promise<boolean> {
+
+    let curUnit: typings.IUnit;
+    const engineCache = ddcsControllers.getEngineCache();
+    const i18n = new I18nResolver(engineCache.i18n, player.lang).translation as any;
+    if (isNaN(Number(player.slot))) {
+        console.log("player doesnt have slotID: " + player);
+        return Promise.resolve(false);
+    } else {
+        const cUnit = await ddcsControllers.unitActionRead({unitId: Number(player.slot)});
+        let message;
+        let currentObjUpdate: any;
+        curUnit = cUnit[0];
+        if (curUnit.inAir) {
+            const unitExist = await ddcsControllers.unitActionRead({_id: "AI|" + itemObj.name + "|"});
+            if (unitExist.length > 0 && warBondItem === "Tanker") {
+                message = "G: " + i18n.TANKERSPAWNALREADYEXISTS;
+                await ddcsControllers.sendMesgToGroup(
+                    player,
+                    curUnit.groupId,
+                    message,
+                    5
+                );
+                return false;
+            } else {
+                if (player.side === 1) {
+                    if (player.redWarBonds >= warBondCost) {
+                        currentObjUpdate = {
+                            _id: player._id,
+                            redWarBonds: player.redWarBonds - warBondCost
+                        };
+                        await ddcsControllers.srvPlayerActionsUpdate(currentObjUpdate);
+                        message = "G: " + i18n.YOUHAVESPENTWARBONDS.replace("#1", i18n[1])
+                            .replace("#2", warBondCost).replace("#3", warBondItem).replace("#4", currentObjUpdate.redWarBonds);
+                        await ddcsControllers.sendMesgToGroup(
+                            player,
+                            curUnit.groupId,
+                            message,
+                            5
+                        );
+                        return true;
+                    } else {
+                        message = "G: " + i18n.YOUDONOTHAVEENOUGHWARBONDS.replace("#1", i18n[1])
+                            .replace("#2", warBondCost).replace("#3", warBondItem).replace("#4", player.redWarBonds);
+                        await ddcsControllers.sendMesgToGroup(
+                            player,
+                            curUnit.groupId,
+                            message,
+                            5
+                        );
+                        return false;
+                    }
+                } else {
+                    if (player.blueWarBonds >= warBondCost) {
+                        currentObjUpdate = {
+                            _id: player._id,
+                            blueWarBonds: player.blueWarBonds - warBondCost
+                        };
+                        await ddcsControllers.srvPlayerActionsUpdate(currentObjUpdate);
+                        message = "G: " + i18n.YOUHAVESPENTWARBONDS.replace("#1", i18n[2])
+                            .replace("#2", warBondCost).replace("#3", warBondItem).replace("#4", currentObjUpdate.blueWarBonds);
+                        await ddcsControllers.sendMesgToGroup(
+                            player,
+                            curUnit.groupId,
+                            message,
+                            5
+                        );
+                        return true;
+                    } else {
+                        message = "G: " + i18n.YOUDONOTHAVEENOUGHWARBONDS.replace("#1", i18n[2])
+                            .replace("#2", warBondCost).replace("#3", warBondItem).replace("#4", player.blueWarBonds);
+                        await ddcsControllers.sendMesgToGroup(
+                            player,
+                            curUnit.groupId,
+                            message,
+                            5
+                        );
+                        return false;
+                    }
+                }
+            }
+        } else {
+            message = "G: " + i18n.YOUCANNOTSPENDRSPOINTSONGROUND;
+            await ddcsControllers.sendMesgToGroup(
+                player,
+                curUnit.groupId,
+                message,
+                5
+            );
+            return false;
+        }
+    }
+}
