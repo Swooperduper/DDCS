dd3d190de0605cf95cbcb98c74dd634ea2e49dc2
kirkwood364 <jamesk1@hotmail.co.uk>
2022-01-03 16:34:40 +0000

    Update oneSec.ts

--- src/controllers/timedEvents/oneSec.ts
+++ src/controllers/timedEvents/oneSec.ts
@@ -12,16 +12,19 @@ export async function processOneSecActions(fullySynced: boolean) {
         }
         const totalDisconnects = await ddcsControllers.simpleStatEventActionsReadDisconnectsInLastSeconds(iCurObj)
         if(totalDisconnects.length > 2){
-            console.log("There were a total of disconnects", totalDisconnects.length, "in the past", iCurObj.secondsAgo,"seconds.")
+            console.log("Clients Disconnected en masse - There were a total of disconnects", totalDisconnects.length, "in the past", iCurObj.secondsAgo,"seconds.")
             const mesg = "**Clients Disconnected en masse** \n DCS.exe stopped sending network traffic for a time \n LP will be refunded \n DCS.log:"
             ddcsControllers.sendMessageToDiscord(mesg);
             for (const player of totalDisconnects){
-                console.log(player._id)
                 let iCurObj =   {_id: player._id,
                                 showInChart : false,
-                }
-                await ddcsControllers.simpleStatEventActionUpdate(iCurObj)
-                console.log("player.name:", player.iName);
+                };
+                await ddcsControllers.simpleStatEventActionUpdate(iCurObj);
+            }
+            const playerArray = await ddcsControllers.srvPlayerActionsRead({sessionName: ddcsControllers.getSessionName()});
+            for (const player of totalDisconnects){
+                let iPlayer = _.find(playerArray, {name: player.iName});
+                console.log (iPlayer)
             }
         }
     }
