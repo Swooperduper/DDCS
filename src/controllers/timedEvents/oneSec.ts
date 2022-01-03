d4e7f85d3562300a65c40e98482c2a81abdc5337
kirkwood364 <jamesk1@hotmail.co.uk>
2022-01-03 15:53:09 +0000

    Update oneSec.ts

--- src/controllers/timedEvents/oneSec.ts
+++ src/controllers/timedEvents/oneSec.ts
@@ -16,6 +16,7 @@ export async function processOneSecActions(fullySynced: boolean) {
             const mesg = "**Clients Disconnected en masse** \n DCS.exe stopped sending network traffic for a time \n LP will be refunded \n DCS.log:"
             ddcsControllers.sendMessageToDiscord(mesg);
             for (const player of totalDisconnects){
+                console.log(player._id)
                 let iCurObj =   {_id: "ObjectId("+player._id+")",
                                 showInChart : false,
                 }
