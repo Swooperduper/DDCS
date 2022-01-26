632063a23616bbe07422900941ae06ce23e931da
Swooperduper <92326642+Swooperduper@users.noreply.github.com>
2022-01-25 18:38:09 -0600

    Update menuUpdate.ts

--- src/controllers/menu/menuUpdate.ts
+++ src/controllers/menu/menuUpdate.ts
@@ -39,7 +39,7 @@ export async function spawnNewMenuCategory(
             let curUnitDictionary = ddcsControllers.getEngineCache().unitDictionary.filter(
                 (unit: any) => unit.type === curSubMenu.cmdProp.type
             );
-            let curwarbondCost = curUnitDictionary.warbondCost;
+            console.log(curUnitDictionary);
             let spawnAmount = 1;
             if (curUnitDictionary.length === 1) {
                 spawnAmount = curUnitDictionary[0].config[ddcsControllers.getEngineCache().config.timePeriod].spawnCount;
@@ -72,7 +72,7 @@ export async function spawnNewMenuCategory(
             if (curSubMenu.cmdProp.mass) {
                 curMenuArray[0] += massTypeString;
             }
-            const curCrates = (curSubMenu.cmdProp.crates) ? "(" + spawnAmount + "Q-" + curSubMenu.cmdProp.crates + "C)" : "";
+            const curCrates = (curSubMenu.cmdProp.crates) ? "(" + spawnAmount + "Q =" curwarbondCost + "WBs and:" curSubMenu.cmdProp.crates + "C)" : "";
 
             // tslint:disable-next-line:max-line-length
             menuSpawnArray.push(`missionCommands.addCommandForGroup(${playerUnit.groupId},"${curSubMenu.itemTitle}${curCrates}",{"${curMenuArray.join('","')}"},sendRequest,${cmdProps})`);
