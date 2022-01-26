22a6860767a8934232e96ee0eaf92768496679c3
Swooperduper <92326642+Swooperduper@users.noreply.github.com>
2022-01-25 18:55:01 -0600

    Update menuUpdate.ts

--- src/controllers/menu/menuUpdate.ts
+++ src/controllers/menu/menuUpdate.ts
@@ -39,7 +39,7 @@ export async function spawnNewMenuCategory(
             let curUnitDictionary = ddcsControllers.getEngineCache().unitDictionary.filter(
                 (unit: any) => unit.type === curSubMenu.cmdProp.type
             );
-            let curwarbondCost = curUnitDictionary.warbondCost;
+            console.log(curUnitDictionary);
             let spawnAmount = 1;
             if (curUnitDictionary.length === 1) {
                 spawnAmount = curUnitDictionary[0].config[ddcsControllers.getEngineCache().config.timePeriod].spawnCount;
